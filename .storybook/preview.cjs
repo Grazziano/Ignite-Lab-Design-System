89d757cbe78dee6e958eb29ed187238fb2f3dc35
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-12 13:46:10 -0300

    Trocando tema storybook e removendo arquivos

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,9 +1,14 @@
+import { theme } from '@storybook/theming';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  docs: {
+    theme: theme.dark,
+  },
+};
